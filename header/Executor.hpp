7a719973456d46ce93a349d058d9e37157655fa4
anthonyp1412 <apan016@ucr.edu>
2020-02-13 15:07:25 -0800

    added executor

---
+++ header/Executor.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <boost/tokenizer.hpp>
+#include <vector>
+#include <sys/types.h>
+using namespace std;
+using namespace boost;
+
+int main()
+{
+        string text = "ls -a -l echo";
+        vector<std::string> commandVector;
+        char_separator<char> sep(" ");
+        tokenizer<char_separator<char>> tokens(text, sep);
+        std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (commandVector));
+
+
+        std::vector<char *> commands(commandVector.size() + 1);
+
+        for (std::size_t i = 0; i != commandVector.size(); i++)
+        {
+                commands[i] = &commandVector[i][0];
+        }
+
+
+        int status = execvp(commands[0], commands.data());
+        return 0;
+}
+
+
