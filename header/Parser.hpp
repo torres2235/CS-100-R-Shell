5e4b2b9b0a33d3c8b94319f0ac4df17507ff692c
Torres2235 <Torres2235@gmail.com>
2020-02-13 14:13:09 -0800

    updated parser to split string up between the connectors

--- header/Parser.hpp
+++ header/Parser.hpp
@@ -1,10 +1,78 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
 #include <vector>
+#include <string>
 
-vector<char>* Parser(string userInput) {
-	
-	string sample = "echo hello && mkdir || echo goodbye";
-	tokenizer<> tok(sample);
+using namespace std;
+
+std::string parser() {
+    
+  std::vector <std::string> subStrings;
+  std::string sub;
+  std::string s = "echo hello || ls -l && mkdir easy; echo hello again; ls -a -l";
+  int index = 0;
+  int length = 0;
+  
+  for(int i = 0; i<= s.length(); i++) {
+      length++;
+          if (s[i] == '&' && s[i-1] == '&') { //checking for "&&"
+              sub = s.substr(index, length-2);
+              
+              subStrings.push_back(sub);
+              
+              if (i != s.length()) {
+                  index = i;
+              }
+              
+              sub = s.substr(index-1, 2);
+              subStrings.push_back(sub);
+              index++;
+              length = 0;
+          }
+         
+      
+          if (s[i] == '|' && s[i-1] == '|') { //checking for "||"
+              sub = s.substr(index, length-2);
+              
+              subStrings.push_back(sub);
+              
+              if (i != s.length()) {
+                  index = i;
+              }
+              
+              sub = s.substr(index-1, 2);
+              subStrings.push_back(sub);
+              index++;
+              length = 0;
+          }
+      
+          if (s[i] == ';') { //checking for ";"
+              sub = s.substr(index, length-1);
+              
+              subStrings.push_back(sub);
+              
+              if (i != s.length()) {
+                  index = i;
+              }
+              
+              sub = s.substr(index, 1);
+              subStrings.push_back(sub);
+              index++;
+              length = 0;
+          }
+      
+      
+          if (i == s.length()) {
+              sub = s.substr(index, length);
+              subStrings.push_back(sub);
+          }
+       
+       
+  }
+  
+  for(auto i = subStrings.begin(); i != subStrings.end(); i++) {
+        std::cout << *i << std::endl;
+  }
+
+  return 0;
 }
+
