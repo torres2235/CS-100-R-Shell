874116037405cd44f2f1d5cf60b1008532ae0312
anthonyp1412 <apan016@ucr.edu>
2020-02-13 21:27:03 -0800

    changed executor

--- header/Executor.hpp
+++ header/Executor.hpp
@@ -6,25 +6,29 @@
 using namespace std;
 using namespace boost;
 
-int main()
+class Executor(vector<string> userInput)
 {
-        string text = "ls -a -l echo";
-        vector<std::string> commandVector;
-        char_separator<char> sep(" ");
-        tokenizer<char_separator<char>> tokens(text, sep);
-        std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (commandVector));
+	private:
+		string input;
+		vector<std::string> exec;
+	public:
+		Executor(vector<string> userInput) {input = userInput;}
+      	        char_separator<char> delimiter(" ");
+
+        tokenizer<char_separator<char>> tokens(input, delimiter);
+        std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (exec));
 
 
-        std::vector<char *> commands(commandVector.size() + 1);
+        std::vector<char *> commands(exec.size() + 1);
 
-        for (std::size_t i = 0; i != commandVector.size(); i++)
+        for (std::size_t i = 0; i != exec.size(); i++)
         {
-                commands[i] = &commandVector[i][0];
+                commands[i] = &exec[i][0];
         }
 
 
-        int status = execvp(commands[0], commands.data());
-        return 0;
+        return int status = execvp(commands[0], commands.data());
+        
 }
 
 
