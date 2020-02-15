557422c8fe42b883e797c2420e93e8917894540e
anthonyp1412 <apan016@ucr.edu>
2020-02-14 21:18:48 -0800

    modified executor

--- src/Executor.cpp
+++ src/Executor.cpp
@@ -10,21 +10,26 @@ using namespace std;
 using namespace boost;
 
 
-		Executor::Executor(vector<string> userInput) {
-			input = userInput[0];
-      	        	char_separator<char> delimiter(" ");
+Executor::Executor(vector<string> userInput) {
+	input = userInput[0];
+	char_separator<char> delimiter(" ");
 
-        		tokenizer<char_separator<char>> tokens(input, delimiter);
-        		std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (exec));
-
-
-        		std::vector<char *> commands(exec.size() + 1);
-
-        		for (std::size_t i = 0; i != exec.size(); i++)
-        		{
-                		commands[i] = &exec[i][0];
-        		}
+			
+	for (int i = 0; i < userInput.size() + 1; i++) {
+		input = userInput[i];
+		cout << input;
+        	tokenizer<char_separator<char>> tokens(input, delimiter);
+        	std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (exec));
 
 
-        		execvp(commands[0], commands.data());
+		std::vector<char *> commands(exec.size() + 1);
+
+        	for (std::size_t i = 0; i != exec.size(); i++)
+        	{
+			commands[i] = &exec[i][0];
 		}
+
+
+        	execvp(commands[0], commands.data());
+	}
+}
