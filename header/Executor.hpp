41a78b7ee5467c64d2f46037cfc4c4641ed2f832
Torres2235 <Torres2235@gmail.com>
2020-02-14 17:45:25 -0800

    creating executor's hpp and cpp

--- header/Executor.hpp
+++ header/Executor.hpp
@@ -1,34 +1,23 @@
+#ifndef __EXECUTOR_HPP__
+#define __EXECUTOR_HPP__
+
 #include <iostream>
 #include <string>
 #include <boost/tokenizer.hpp>
 #include <vector>
 #include <sys/types.h>
+
 using namespace std;
 using namespace boost;
 
-class Executor(vector<string> userInput)
+class Executor()
 {
 	private:
 		string input;
 		vector<std::string> exec;
 	public:
-		Executor(vector<string> userInput) {input = userInput;}
-      	        char_separator<char> delimiter(" ");
-
-        tokenizer<char_separator<char>> tokens(input, delimiter);
-        std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (exec));
-
-
-        std::vector<char *> commands(exec.size() + 1);
-
-        for (std::size_t i = 0; i != exec.size(); i++)
-        {
-                commands[i] = &exec[i][0];
-        }
-
-
-        return int status = execvp(commands[0], commands.data());
+		Executor(vector<string> userInput);
         
 }
 
-
+#endif // __EXECUTOR_HPP__
---
+++ src/Executor.cpp
@@ -0,0 +1,39 @@
+#ifndef __EXECUTOR_CPP__
+#define __EXECUTOR_CPP__
+
+#include <iostream>
+#include <string>
+#include <boost/tokenizer.hpp>
+#include <vector>
+#include <sys/types.h>
+using namespace std;
+using namespace boost;
+
+class Executor()
+{
+	private:
+		string input;
+		vector<std::string> exec;
+	public:
+		Executor(vector<string> userInput) {
+			input = userInput;
+      	        	char_separator<char> delimiter(" ");
+
+        		tokenizer<char_separator<char>> tokens(input, delimiter);
+        		std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (exec));
+
+
+        		std::vector<char *> commands(exec.size() + 1);
+
+        		for (std::size_t i = 0; i != exec.size(); i++)
+        		{
+                		commands[i] = &exec[i][0];
+        		}
+
+
+        		execvp(commands[0], commands.data());
+		}
+        
+}
+
+#endif //__EXECUTOR_CPP__
