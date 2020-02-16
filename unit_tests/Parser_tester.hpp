6928459f0159f80fb6b51c771d35cf578f5d2852
anthonyp1412 <apan016@ucr.edu>
2020-02-15 22:44:38 -0800

    fixed parser tests

--- unit_tests/Parser_tester.hpp
+++ unit_tests/Parser_tester.hpp
@@ -11,48 +11,56 @@
 
 TEST(ParserTester, ParseShortString) {
 	Parser* str = new Parser("echo hello");
+	str->parserLogic();
 	vector<string> parsed = str->getVector();
 	EXPECT_EQ(parsed.at(0), "echo hello");
 }
 
 TEST(ParserTester, ParseConnectors) {
 	Parser* str = new Parser("echo hello && echo goodbye");
+	str->parserLogic();
 	vector<string> parsed = str->getVector();
 	EXPECT_EQ(parsed.at(1), "&&" );
 }
 
 TEST(ParserTester, ParseConnectors2) {
 	Parser* str = new Parser("echo hello || echo goodbye");
+	str->parserLogic();
         vector<string> parsed = str->getVector();
         EXPECT_EQ(parsed.at(1), "||");
 }
 
 TEST(ParserTester, ParseConnectors3) {
 	Parser* str = new Parser("echo hello; echo goodbye");
+	str->parserLogic();
         vector<string> parsed = str->getVector();
         EXPECT_EQ(parsed.at(1), ";");
 }
 
 TEST(ParserTester, ParseLongString) {
 	Parser* str = new Parser("mkdir && echo hello || echo goodbye;");
+	str->parserLogic();
         vector<string> parsed = str->getVector();
         EXPECT_EQ(parsed.at(2), " echo hello ");
 }
 
 TEST(ParserTester, ParseLongString2) {
 	Parser* str = new Parser("mkdir && echo hello || echo goodbye;");
+	str->parserLogic();
         vector<string> parsed = str->getVector();
         EXPECT_EQ(parsed.at(3), "||");
 }
 
 TEST(ParserTester, ParseLongString3) {
 	Parser* str = new Parser("mkdir && echo hello || echo goodbye;");
+	str->parserLogic();
         vector<string> parsed = str->getVector();
         EXPECT_EQ(parsed.at(4), " echo goodbye");
 }
 
 TEST(ParserTester, ParseLongString4) {
 	Parser* str = new Parser("mkdir && echo hello || echo goodbye;");
+	str->parserLogic();
 	vector<string> parsed = str->getVector();
 	EXPECT_EQ(parsed.at(5), ";");
 }
