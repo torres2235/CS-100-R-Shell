a2a52ea59cce74f87c7a080be42d9e123a2c8900
anthonyp1412 <apan016@ucr.edu>
2020-02-15 19:08:51 -0800

    added integration tests

---
+++ integration_tests/exit_command_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Now testing for exit commands"
+../rshell < "exit_command_tests.txt"
---
+++ integration_tests/single_command_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Running single command tests"
+
+../rshell < "single_command_tests.txt"
