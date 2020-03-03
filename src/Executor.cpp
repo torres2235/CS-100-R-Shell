3627baa2caa1f08f50c2838c8f328c7741f2cffc
Torres2235 <Torres2235@gmail.com>
2020-03-02 23:07:49 -0800

    test command works (not with brackets)

--- src/Executor.cpp
+++ src/Executor.cpp
@@ -26,6 +26,7 @@ int Executor::execute(string userInput) {
 	//for (int i = 0; i < userInput.size(); i++) {
 
 		input = userInput;
+		//cout << input << endl;
 
         	tokenizer<char_separator<char>> tokens(input, delimiter);
         	std::copy(tokens.begin(), tokens.end(), std::back_inserter<std::vector<std::string> > (exec));
@@ -34,10 +35,63 @@ int Executor::execute(string userInput) {
 		std::vector<char *> commands(exec.size() + 1);//new vector w/o white space
 
         	for (std::size_t i = 0; i != exec.size(); i++)
-        	{
+        	{ 
 			commands[i] = &exec[i][0];//setting elementsto new vector
 		}
-
+		
+		if (string(commands[0]) == "test") {
+			//cout << commands[0] << " " << commands[1] << " " << commands[2] << endl;
+			if (string(commands[1]) == "-e") {
+				exitStatus = test(commands[2]);
+				if (exitStatus != 0) {
+					cout << "( TRUE )" << endl;
+					return 0;
+				}
+				else {
+					cout << "( FALSE )" <<endl;
+					return 256;
+				}
+				return exitStatus;
+			}
+			else if (string(commands[1]) == "-f") {
+				exitStatus = test(commands[2]);
+				if (exitStatus == 2) {
+					cout << "( TRUE )" << endl;
+					return 0;
+				}
+				else {
+					cout << "( FALSE )" << endl;
+					return 256;
+				}
+				return exitStatus;
+			}
+			else if (string(commands[1]) == "-d") {
+				exitStatus = test(commands[2]);
+				if (exitStatus == 1) {
+					cout << "( TRUE )" << endl;
+					return 0;
+				}
+				else { 
+					cout << "( FALSE )" << endl;
+					return 256;
+				}
+				return exitStatus;
+			}
+			else {
+				exitStatus = test(commands[1]);
+				if (exitStatus != 0) {
+					cout << "( TRUE )" << endl;
+					return 0;
+				}
+				else {
+					cout << "( FALSE )" << endl;
+					return 256;
+				}
+				return exitStatus;
+			}
+		
+		}
+				
 	 //-----------------------------------------
 	 //This section takes the command vector and runs it through execvp()
         	pid_t childPid = fork();
@@ -66,6 +120,7 @@ int Executor::execute(string userInput) {
 
 int Executor::test(char* testInput) {
 	int returnStatus = 0;
+	//std::cout << "hellooooo" <<  std::endl;
 	struct stat test;
 	stat(testInput, &test);
 	switch (test.st_mode & S_IFMT) {
